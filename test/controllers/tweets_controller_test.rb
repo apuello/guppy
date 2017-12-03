7724539b653f43817a36517c379a52238cdb6050
Avelino Puello <avelinopuello@gmail.com>
2017-12-02 17:50:55 -0800

    testing...

--- test/controllers/tweets_controller_test.rb
+++ test/controllers/tweets_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class TweetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tweets_index_url
-    assert_response :success
+  test "get tweets with coke, diet-cola and coca-cola" do
+  	get tweets_url 
+  	
+	assert_select 'title', "Guppy"
+	assert_select 'h1', "Welcome to Guppy:"
+	assert_select "tr" do
+	  assert_select "td", 4
+	end
   end
-
 end
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TweetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "check model" do
+  	tweet = Tweet.last
+    assert 2, tweet.user_id
+    assert "user_two", tweet.user_handle
+    assert 0.9, tweet.sentiment
+    assert 300, tweet.followers
+  end
 end
