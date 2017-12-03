8cd29b0dca9011b42f3030729b4e476888901ae6
Avelino Puello <avelinopuello@gmail.com>
2017-12-02 16:43:48 -0800

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..5).each do |loop|
+	# user_id | user_handle | message | sentiment | followers 
+	Tweet.create({ user_id: loop, user_handle: "user_#{loop}", message: "Hi there, coke #{loop}", sentiment: rand(-1..1), followers: rand(20..300)})
+end
+
+(1..5).each do |loop|
+	# user_id | user_handle | message | sentiment | followers 
+	Tweet.create({ user_id: loop * 6, user_handle: "user_#{loop}", message: "Hi there, coca-cola #{loop * 6}", sentiment: rand(-1..1), followers: rand(20..300)})
+end
+
+(1..5).each do |loop|
+	# user_id | user_handle | message | sentiment | followers 
+	Tweet.create({ user_id: loop * 10, user_handle: "user_#{loop}", message: "Hi there, diet-cola #{loop * 10}", sentiment: rand(-1..1), followers: rand(20..300)})
+end
